410b7135644036addcfd4483a6acb98b43df6366
mosfetNik <adityarai424@gmail.com>
2024-07-23 16:11:47 +0530

    complete the few card

---
+++ src/components/product-cart.tsx
@@ -0,0 +1,37 @@
+import { FaPlus } from "react-icons/fa";
+
+type ProductProps = {
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  handlder: () => void;
+};
+const ProductCard = ({
+  productId,
+  photo,
+  name,
+  price,
+  stock,
+  handlder,
+}: ProductProps) => {
+  const server = "dummyServer";
+
+  return (
+    <>
+      <div className="product-card">
+        <img src={photo} alt={name} />
+        <p>{name}</p>
+        <span> ₹ {price}</span>
+
+
+        <div>
+          <button onClick={()=>handlder()}><FaPlus /></button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,27 @@
-
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-cart";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  const addToCartHandler =()=>{
+
+  }
+  return (
+    <div className="home">
+      <section></section>
+
+      <h1>
+        Latest Product
+        <Link to="/search" className="findmore">
+          MOre{" "}
+        </Link>
+      </h1>
+
+      <main>
+        <ProductCard productId="afdfdaf" name="macbook" price={200} stock ={10}  handlder={addToCartHandler} photo="https://m.media-amazon.com/images/I/71jG+e7roXL._SX679_.jpg" />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
