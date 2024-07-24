494754a94a0f3d06f873cd917d52cc73c08fc785
universeBuddy <rai358788@gmail.com>
2024-07-24 20:07:27 +0530

    near to complete cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,4 +1,22 @@
-const cartItem = [];
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItem from "./cart-item";
+
+
+
+
+const cartItem = [
+
+  {
+    productId:"fdfsfsdff",
+    photo:"https://m.media-amazon.com/images/I/71jG+e7roXL._SX679_.jpg",
+    name:"MacBook",
+    quantity:4,
+    stock:10,
+  }
+
+  
+];
 const subtotal = 4000;
 const tax = Math.round(subtotal * 0.18);
 const shippingCharges = 200;
@@ -6,19 +24,61 @@ const discount = 400;
 const total = subtotal + tax + shippingCharges;
 
 const Cart = () => {
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      if (Math.random() > 0.5) setIsValidCouponCode(true);
+      else setIsValidCouponCode(false);
+    }, 1000);
+    return () => {
+      clearTimeout(timeOutId);
+      setIsValidCouponCode(false);
+    };
+  }, [couponCode]);
+
   return (
     <div className="cart">
-      <main></main>
+      <main>
+
+{cartItem.map((i,idx) => (<CartItem  key={idx} cartItem={i}/>
+
+
+))}
+
+
+      </main>
       <aside>
         <p>Subtotal: {subtotal}</p>
         <p>Shipping Charges: {shippingCharges}</p>
         <p>Tax : {tax}</p>
         <p>
-          Discount-<em> {discount}</em>{" "}
+          Discount-<em className="red"> {discount}</em>{" "}
         </p>
         <p>
           <strong>Total : {total}</strong>
         </p>
+
+        <input
+          type="text"
+          placeholder="Coupon Code"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              {" "}
+              Rs {discount} off using the <code> {couponCode}</code>{" "}
+            </span>
+          ) : (
+            <span className="red">
+              {" "}
+              Invalid Coupon <VscError />
+            </span>
+          ))}
       </aside>
       Cart
     </div>
