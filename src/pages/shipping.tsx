a2e4321a972f0b44146600b303e963cb2e04b42d
universeBuddy <rai358788@gmail.com>
2024-07-25 21:46:30 +0530

    shipping component complete

--- src/pages/shipping.tsx
+++ src/pages/shipping.tsx
@@ -1,58 +1,81 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useState } from "react";
 import { BiArrowBack } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
 
-const Shipping= () => {
+const Shipping = () => {
 
-  const  [shippingInfo,setShippingInfo] =useState({
-    address:"",
-    city:"",
-    state:"",
-    country:"",
-    pinCode:""
+  const navigate = useNavigate();
+  const [shippingInfo, setShippingInfo] = useState({
+    address: "",
+    city: "",
+    state: "",
+    country: "",
+    pinCode: "",
+  });
 
-  })
+  const changeHandler = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => { 
+    setShippingInfo((prev) => ({ ...prev, [e.target.name] : e.target.value }));
+  };
 
-
-
-  const changeHandler =(e:ChangeEvent<HTMLInputElement>)=>{
-   
-    setShippingInfo(prev=>({...prev,[e.target.name]:e.target.value} ));
-  }; 
-
-
-
-  return (
+  return ( 
     <div className="shipping">
-      <button><BiArrowBack /></button>
+      <button className="back-btn" onClick={()=> navigate("/cart")}>
+        <BiArrowBack />
+      </button>
 
+      <form>
+        <h1>Shipong Address</h1>
+        <input
+          required
+          type="text"
+          placeholder="Address"
+          name="address"
+          value={shippingInfo.address}
+          onChange={changeHandler}
+        />
 
+        <input
+          required
+          type="text"
+          placeholder="city"
+          name="city"
+          value={shippingInfo.city}
+          onChange={changeHandler}
+        />
 
-<form>
+        <input
+          required
+          type="text"
+          placeholder="state"
+          name="state"
+          value={shippingInfo.state}
+          onChange={changeHandler}
+        />
 
-  <h1>Shipong Address</h1>
-  <input required type="text" placeholder="Address" name="address"  value={shippingInfo.address} onChange = {changeHandler}/> 
+        <select
+          required
+          name="country"
+          value={shippingInfo.country}
+          onChange={changeHandler}
+        >
+          <option value=" ">Choose Country</option>
+          <option value="in">India</option>
+          <option value="in">U.S.A</option>
+        </select>
 
-  <input required type="text" placeholder="city" name="city"  value={shippingInfo.city} onChange = {changeHandler}/>
-
-  <input required type="text" placeholder="state" name="state"   value={shippingInfo.state} onChange = {changeHandler} />
-
-  <select required   name="country"  value={shippingInfo.country} onChange = {changeHandler} >
-   
-   <option value= " " >Choose Country</option>
-   <option value= "in" >India</option>
-   <option value= "in" >U.S.A</option>
-  
-  </select>
-
-  <input required type="number" placeholder="pin code" name="pinCode"  value={shippingInfo.pinCode} onChange = {changeHandler}/>
-
-</form> 
-
-
-
+        <input
+          required
+          type="number"
+          placeholder="pin code"
+          name="pinCode"
+          value={shippingInfo.pinCode}
+          onChange={changeHandler}
+        />
+        <button type="button">Pay Now</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Shipping
+export default Shipping;
